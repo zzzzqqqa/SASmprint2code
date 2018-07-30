7890532f1f77379e296d855f218eee7f57e0da6e
zzzzqqqa <zzzzqqq@gmail.com>
2018-07-30 16:56:16 +0800

    bugfix remove code with leading numbers

--- processpaste.js
+++ processpaste.js
@@ -1,10 +1,4 @@
 function processpaste (elem, savedcontent) {
-   // pasteddata = elem.innerText;
-    //^^Alternatively loop through dom (elem.childNodes or elem.getElementsByTagName) here
-//alert(pasteddata);
- //   elem.innerText = savedcontent;
-
-    // Do whatever with gathered data;
     var regex = /mpflag.*?(?=(mpflag|otherflag))/gm;
     var mp,result;
     var member="";
@@ -12,28 +6,15 @@ function processpaste (elem, savedcontent) {
 	s = elem.innerText;
 	s = s.replace(/.*The SAS System.*|[0-9 ]{10}\+.*/g,"");
 	s = s.replace(/\nMPRINT\([A-Z0-9_\.]+\):/g,"mpflag");
-	s = s.replace(/\nMACROGEN|\nMLOGIC|\nNOTE|\nSYMBOLGEN:|\nWARNING|\nERROR|\n[0-9] \+ /g,"otherflag");
-	s = s.replace(/(\r\n|\n|\r)/g,"");
-
+	s = s.replace(/\nMACROGEN|\nMLOGIC|\nNOTE|\nSYMBOLGEN:|\nWARNING|\nERROR|\n[0-9]+ \+/g,"otherflag");
+	s = s.replace(/(\r\n|\n|\r)/g," ");
     result = s.match(regex);
 	if (result) {
 		for (var i = 0; i < result.length; i++) {
         mp = result[i].replace("mpflag","");
-         mp = mp.replace(/;[^;]*$/,"; \r\n<br/>");  
+         mp = mp.replace(/;.*$/,"; \r\n<br/>");  
         member = member + mp ; 
 		}
- //   w.value=member;  
 	}
-//	var oTable=document.getElementById("div"); 
-//	oTable.innerText=strip(member);
-//		oTable.innerText.replace("&gt;", ">");
-//        oTable.innerText.replace("&lt;", "<");
-//        oTable.innerText.replace("&nbsp;", " ");
-//        oTable.innerText.replace("&amp;", "&");
-//        oTable.innerText.replace("&quot;", "\"");
-//        oTable.innerText.replace("'", "\'");
-//        oTable.innerText.replace("<br/>", "\n");
-// document.body.getElementById('div').value=member;
 	document.body.innerText=strip(member);
-    
 } 
